b25642c30e3d97577e089be8d643a10453d9f585
Felipe <felipeshonorato@gmail.com>
2022-06-20 15:54:53 -0300

    feat: Calculando IMC com condicionais.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,24 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nova Nutrição";
 
 var paciente = document.querySelector("#primeiro-paciente");
-
 var pacientePeso = document.querySelector(".info-peso").textContent;
-
-console.log(pacientePeso);
-
 var pacienteAltura = document.querySelector(".info-altura").textContent;
+var imcPaciente = pacientePeso / (pacienteAltura * pacienteAltura);
 
-console.log(pacienteAltura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-var imc = pacientePeso / (pacienteAltura * pacienteAltura);
-console.log(imc);
+if (pacientePeso < 0 || pacientePeso > 1000) {
+  console.log("Peso Inválido!");
+  pesoEhValido = false;
+}
+
+if (pacienteAltura < 0 || pacienteAltura > 3.0) {
+  console.log("Altura Inválida!");
+  alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+  var imc = document.querySelector(".info-imc");
+  imc.textContent = imcPaciente;
+}
