31d887ef0aa2780f91e87adafbe8a8479f344895
Felipe <felipeshonorato@gmail.com>
2022-06-24 14:45:26 -0300

    feat: Adicionado botão para remover paciente.

--- js/insere-linha-paciente.js
+++ js/insere-linha-paciente.js
@@ -11,6 +11,7 @@ function criarNovoTr(paciente) {
   var alturaTd = criarNovoTd(paciente.altura, "info-altura");
   var gorduraTd = criarNovoTd(paciente.gordura, "info-gordura");
   var imcTd = criarNovoTd(paciente.imc, "info-imc");
+  var remover = criarNovoTdBotao();
 
   //Inserimos as tds criadas dentro da tr criada
   pacienteTr.appendChild(nomeTd);
@@ -18,14 +19,29 @@ function criarNovoTr(paciente) {
   pacienteTr.appendChild(alturaTd);
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
+  pacienteTr.appendChild(remover);
 
   return pacienteTr;
 }
 
+//Função para criar um td com dados do paciente
 function criarNovoTd(dado, classe) {
   var td = document.createElement("td");
   td.textContent = dado;
   td.classList.add(classe);
+
+  return td;
+}
+
+//Função para criar um td para botão remover
+function criarNovoTdBotao() {
+  var td = document.createElement("td");
+  var button = document.createElement("button");
+  button.innerHTML = "Remover";
+  button.classList.add("botao-remover");
+  button.setAttribute("onclick", "removerPaciente(event.target)");
+  td.appendChild(button);
+  td.classList.add("info-remove");
 
   return td;
 }
--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -1,4 +1,4 @@
-// Função para remover um paciente da tabela pacientes
+// Função para remover um paciente da tabela pacientes através de dois clicks
 var pacientes = document.querySelectorAll(".paciente");
 
 //Selecionamos a tabela para ser responsável em delegar a ação
@@ -13,3 +13,12 @@ tabela.addEventListener("dblclick", function (event) {
     event.target.parentNode.remove();
   }, 500);
 });
+
+// Função para remover um paciente da tabela pacientes através do botão remover
+function removerPaciente(elementoClicado) {
+  elementoClicado.closest("tr").classList.add("fadeOut");
+
+  setTimeout(function () {
+    elementoClicado.closest("tr").remove();
+  }, 500);
+}
